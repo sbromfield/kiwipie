619c5c0b01299cadea017b155c8994cb7bc9fba7
Stephen Bromfield <s.bromfield@gmail.com>
2016-06-09 10:49:02 -0400

    small change to rss url
    need to figure out if this is a problem. filter_var wasn't validating
    the rss url

--- controllers/rsscontroller.php
+++ controllers/rsscontroller.php
@@ -12,8 +12,10 @@ class rsscontroller
 		if(auth::isloggedin() && auth::isadmin())
 		{
 
-			$rss = filter_var($_GET['rss'], FILTER_VALIDATE_URL);
-
+			//$rss = filter_var($_GET['rss'], FILTER_VALIDATE_URL);
+			//need to figure out if this is a problem. filter_var wasn't validating the rss url
+			$rss = $_GET['rss'];
+			
 			if( ! is_bool($rss))
 			{
 				$num = classes::getcourseid($_SESSION['courseid']);

3195ef1057efe21d747806bd6f513fbda2f6745a
Stephen Bromfield <s.bromfield@gmail.com>
2017-02-12 00:30:24 -0500

    Update xml2.php
    Changes to use the date field in the db.

--- cron/xml2.php
+++ cron/xml2.php
@@ -27,6 +27,13 @@ for($i = $len ; $i > -1; $i--)
 
 	$classtitle = $result['items'][$i]['title'];
 	
+	$date = $result['items'][$i]['pubDate'];
+	$r = strpos($date, ":");
+	$date = substr($date, 0, $r - 3); 
+	$d = DateTime::createFromFormat("D, d M Y", $date);
+	
+	//echo $d->format("Y-m-d H:i:s"). "<br>";
+	
 	$doc = new DOMDocument();
 	$doc->load($classes);
 
@@ -45,13 +52,12 @@ for($i = $len ; $i > -1; $i--)
 	$all = classes::getclasses($courseid,$video);
 	if($all == NULL)
 	{	
-		classes::insertclass($video, $classtitle, $courseid, $numbooks);
+		classes::insertclass($video, $classtitle, $courseid, $numbooks, $d->format("Y-m-d H:i:s") );
 	}	
 }
 
 
 }
-
 
 
 ?>
